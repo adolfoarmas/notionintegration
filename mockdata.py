0a0787d306e15aa4d0c3cd54de4047ecb8b2957b
adolfo armas <adolfoarmas.90@gmail.com>
2023-02-25 17:15:55 -0300

    add media and mock example Page data file

---
+++ mockdata.py
@@ -0,0 +1,22 @@
+"""
+mock_data_to_save = [
+            
+            {'Book Title': 'Tucusito',
+            'Author': 'Andres Bello',
+            'Type of Element': {'id':"highlight", 'name':'🖍Highlight'},
+            'page': 48,
+            'Position': "250-260",
+            'Highlignted Text': "lorem ipsum ... lorem ipsum ...",
+            'Date': "2023-02-01"},
+
+            {'Book Title': 'Ingles 7',
+            'Author': 'Maria Latorraca',
+            'Type of Element': {'id':"marker", 'name':'📑Marker'},
+            'page': 70,
+            'Position': "50-160",
+            'Highlignted Text': "lorem ipsum ... lorem ipsum ...",
+            'Date': "2022-06-01"}
+
+            ]
+
+"""
